b8668929435a61d775c8e2bd4ef00577a23776ad
EMMERSON LISBOA DE ALMEIDA <111537291+lisboabh09@users.noreply.github.com>
2022-10-31 14:13:00 -0300

    RPG

---
+++ RPG/src/FichaRpg.java
@@ -0,0 +1,34 @@
+public class FichaRpg {
+    public static void main(String[] args) {
+        String nomePersonagem = "Vagante Noturno";
+        String raca = "Humano";
+        String classePersonagem = "Guerreiro";
+        
+        int forca = 15;
+        int destreza = 14;
+        int constituicao = 12;
+        int inteligencia = 11;
+        int sabedoria = 9;
+        int carisma = 7;
+        int classeArmadura = 9;
+        int pontosVida = 12;
+
+       System.out.println ("---------------------------------------- ");
+       System.out.println ("Os atributos do seu personagem são:");
+       System.out.println("O nome do Personagem: " + nomePersonagem);
+       System.out.println("A raça do Personagem: " + raca);
+       System.out.println("Este Personagem é da classe: " + classePersonagem);
+       System.out.println ("---------------------------------------- ");
+       System.out.println ("Força: " + forca);
+       System.out.println ("Destreza: " + destreza);
+       System.out.println ("Constituição: " + constituicao);
+       System.out.println ("Inteligencia: " + inteligencia);
+       System.out.println ("Sabedoria: " + sabedoria);
+       System.out.println ("Carisma: " + carisma);
+       System.out.println ("Classe de Amardura: " + classeArmadura);
+       System.out.println ("Pontos de Vida: " + pontosVida);
+
+    }
+
+    }
+

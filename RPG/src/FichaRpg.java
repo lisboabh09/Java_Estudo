5ac9f3f32a5343172d5f43bc22ea38192d8ae5ed
EMMERSON LISBOA DE ALMEIDA <111537291+lisboabh09@users.noreply.github.com>
2023-01-24 10:49:59 -0300

    atualizações nas pastas

--- RPG/src/FichaRpg.java
+++ RPG/src/FichaRpg.java
@@ -1,9 +1,15 @@
+import java.security.spec.EncodedKeySpec;
+
+import javax.lang.model.util.ElementScanner6;
+
 public class FichaRpg {
     public static void main(String[] args) {
         String nomePersonagem = "Vagante Noturno";
         String raca = "Humano";
         String classePersonagem = "Guerreiro";
-        
+
+        //Valor dos atributos do personagem.
+
         int forca = 15;
         int destreza = 14;
         int constituicao = 12;
@@ -13,18 +19,30 @@ public class FichaRpg {
         int classeArmadura = 9;
         int pontosVida = 12;
 
+        //modificadores de atributos
+        int mFor = 1;
+        int mDes = 1;
+        int mCons = 1;
+        int mInt = 1;
+        int mSab = 1;
+        int mCar =1;
+        
+
+
+
        System.out.println ("---------------------------------------- ");
        System.out.println ("Os atributos do seu personagem são:");
-       System.out.println("O nome do Personagem: " + nomePersonagem);
-       System.out.println("A raça do Personagem: " + raca);
-       System.out.println("Este Personagem é da classe: " + classePersonagem);
+       System.out.println ("O nome do Personagem: " + nomePersonagem);
+       System.out.println ("A raça do Personagem: " + raca);
+       System.out.println ("Este Personagem é da classe: " + classePersonagem);
        System.out.println ("---------------------------------------- ");
-       System.out.println ("Força: " + forca);
-       System.out.println ("Destreza: " + destreza);
-       System.out.println ("Constituição: " + constituicao);
-       System.out.println ("Inteligencia: " + inteligencia);
-       System.out.println ("Sabedoria: " + sabedoria);
-       System.out.println ("Carisma: " + carisma);
+       System.out.println ("Atributos do Personagem");
+       System.out.println ("Força: " + forca + mFor);
+       System.out.println ("Destreza: " + destreza + mDes);
+       System.out.println ("Constituição: " + constituicao + mCons);
+       System.out.println ("Inteligencia: " + inteligencia + mInt);
+       System.out.println ("Sabedoria: " + sabedoria + mSab);
+       System.out.println ("Carisma: " + carisma + mCar);
        System.out.println ("Classe de Amardura: " + classeArmadura);
        System.out.println ("Pontos de Vida: " + pontosVida);
 
